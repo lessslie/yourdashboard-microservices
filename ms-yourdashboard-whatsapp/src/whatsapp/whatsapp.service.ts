5acae17b597a355f8786ff17b59512baa4f6f047
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-08-01 21:11:16 -0300

    Configuracion de multiples cuentas de whatsApp

--- ms-yourdashboard-whatsapp/src/whatsapp/whatsapp.service.ts
+++ ms-yourdashboard-whatsapp/src/whatsapp/whatsapp.service.ts
@@ -6,39 +6,40 @@ dotenv.config();
 
 @Injectable()
 export class WhatsappService {
-  private token = process.env.WHATSAPP_TOKEN;
-  private phoneId = process.env.WHATSAPP_PHONE_ID;
+  async sendMessageWithCuenta(
+    cuenta: { phone_number_id: string; token: string },
+    to: string,
+    message: string,
+  ): Promise<any> {
+    const url = `https://graph.facebook.com/v19.0/${cuenta.phone_number_id}/messages`;
 
-  async sendMessage(to: string, message: string): Promise<any> {
-  const url = `https://graph.facebook.com/v19.0/${this.phoneId}/messages`;
+    const headers = {
+      Authorization: `Bearer ${cuenta.token}`,
+      'Content-Type': 'application/json',
+    };
 
-  const headers = {
-    Authorization: `Bearer ${this.token}`,
-    'Content-Type': 'application/json',
-  };
+    const data = {
+      messaging_product: 'whatsapp',
+      to,
+      type: 'text',
+      text: { body: message },
+    };
 
-  const data = {
-    messaging_product: 'whatsapp',
-    to: to,
-    type: 'text',
-    text: { body: message },
-  };
-
-  try {
-    const response = await axios.post(url, data, { headers });
-    return response.data;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error)) {
-      console.error(
-        'Error enviando mensaje:',
-        error.response?.data || error.message,
-      );
-    } else if (error instanceof Error) {
-      console.error('Error inesperado:', error.message);
-    } else {
-      console.error('Error inesperado:', error);
+    try {
+      const response = await axios.post(url, data, { headers });
+      return response.data;
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        console.error(
+          'Error enviando mensaje:',
+          error.response?.data || error.message,
+        );
+      } else if (error instanceof Error) {
+        console.error('Error inesperado:', error.message);
+      } else {
+        console.error('Error inesperado:', error);
+      }
+      throw new Error('No se pudo enviar el mensaje');
     }
-    throw new Error('No se pudo enviar el mensaje');
   }
-}
 }
